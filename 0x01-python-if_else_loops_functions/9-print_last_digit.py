116903dfb21a445fc852c21dd66881988a2e70e1
Bethelhem Ashenafi <bettyashu5@gmail.com>
2022-09-22 04:30:26 -0700

    loops

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
 def print_last_digit(number):
+    """Print the last digit of a number and return it."""
     print(abs(number) % 10, end="")
-    return (abs(number) % 10)i
+    return (abs(number) % 10)
