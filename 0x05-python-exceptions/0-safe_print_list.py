d22a38de10d9d1f49c5543f9a9b9b9073bca394f
Bethelhem Ashenafi <bettyashu5@gmail.com>
2022-10-12 01:14:38 -0700

    exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
+
+
 def safe_print_list(my_list=[], x=0):
-    real = 0
-    if isinstance(my_list, list) and isinstance(x, int):
-        for i in range(x):
-            try:
-                print("{}".format(my_list[i]), end="")
-            except:
-                pass
-            else:
-                real += 1
-    print("")
-    return real
+    elements = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            elements += 1
+        except:
+            pass
+    print()
+    return elements
