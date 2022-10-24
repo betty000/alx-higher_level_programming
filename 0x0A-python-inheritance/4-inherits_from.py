49aead7bb2713c9981326e96ecee558d789c296a
Bethelhem Ashenafi <bettyashu5@gmail.com>
2022-10-24 05:45:29 -0700

    inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
+'''Module for inherits_from method.'''
+
+
 def inherits_from(obj, a_class):
-    """Checks if the object is an instance of a class that inherited\
- directly or indirectly from the specified class
- """
-    if isinstance(obj, a_class) and not type(obj) == a_class:
-        return True
-    else:
-        return False
+    '''Determines if an object is a true subclass of a class.'''
+    return isinstance(obj, a_class) and type(obj) != a_class
