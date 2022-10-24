86697c147b39552409f0b766c71cb645d29f8766
Bethelhem Ashenafi <bettyashu5@gmail.com>
2022-10-24 06:01:11 -0700

    inheritance

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-'''Module for MyInt class.'''
+"""
+Contains definition of class MyInt
+"""
 
 
 class MyInt(int):
+    """Definition of class MyInt that inherits from class int"""
+
     def __eq__(self, other):
-        '''Override equals, inverting it.'''
+        """Overrides equals, inverting it"""
         return int(self) != int(other)
 
     def __ne__(self, other):
-        '''Override not-equals, inverting it.'''
+        """Overrides not-equals, inverting it"""
         return int(self) == int(other)
