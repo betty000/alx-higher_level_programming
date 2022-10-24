489217ced8a2debe011712d57fd94060cb572737
Bethelhem Ashenafi <bettyashu5@gmail.com>
2022-10-24 05:29:19 -0700

    inheritancce

--- 0x0A-python-inheritance/tests/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/tests/3-is_kind_of_class.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
-'''Module for is_kind_of_class method.'''
+"""
+Module for is_same_class method
+"""
 
 
-def is_kind_of_class(obj, a_class):
-    '''Determines if an object is a subclass of a class.'''
-    return isinstance(obj, a_class)
+def is_same_class(obj, a_class):
+    """Method for comparing object classes
+
+    Args:
+        obj (unknown): object whose type is to be checked.
+        a_class (str): class criteria to validate.
+
+    Return:
+        True if obj isinstance of a_class/ class that inherits from it.
+        otherwise False
+
+    """
+
+    if issubclass(type(obj), a_class) or isinstance(obj, a_class):
+        return True
+    return False
