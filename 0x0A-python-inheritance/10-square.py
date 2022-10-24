6cfc2090dd5404ed43008492eb961eebde6fb2f2
Bethelhem Ashenafi <bettyashu5@gmail.com>
2022-10-24 05:55:58 -0700

    inheritance

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,44 +1,16 @@
 #!/usr/bin/python3
-""" 10-square """
-
-
-class BaseGeometry:
-    """ Class BaseGeometry """
-    def area(self):
-        """ raises an exception """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ validates value """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """ Class Rectangle subclass of BaseGeometry"""
-    def __init__(self, width, height):
-        """ class constructor """
-        self.__width = width
-        self.__height = height
-        BaseGeometry.integer_validator(self, "width", width)
-        BaseGeometry.integer_validator(self, "height", height)
-
-    def area(self):
-        return self.__width * self.__height
-
-    def __str__(self):
-        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+'''Module for Rectangle class.'''
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """ Class Rectangle subclass of Rectangle subclass of BaseGeometry"""
+    '''A subclass representing a rectangle.'''
     def __init__(self, size):
-        """subclass constructor"""
+        '''Constructor.'''
+        self.integer_validator("size", size)
         self.__size = size
-        BaseGeometry.integer_validator(self, "size", size)
         super().__init__(size, size)
 
     def area(self):
-        return self.__size * self.__size
+        '''Method for area of square.'''
+        return self.__size ** 2
