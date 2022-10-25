9474fa1d5be365972f95d52d55171f63ecb884f1
Bethelhem Ashenafi <bettyashu5@gmail.com>
2022-10-25 13:26:44 -0700

    input_output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""Script that add all arguments to a Python list
-"""
+"""add item"""
+
+
+import json
 import sys
-load = __import__('6-load_from_json_file').load_from_json_file
-save = __import__('5-save_to_json_file').save_to_json_file
+import os.path
 
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    pythonList = load("add_item.json")
-
-except(TypeError, FileNotFoundError):
-    pythonList = []
-
-for i in sys.argv[1:]:
-    pythonList.append(i)
-
-save(pythonList, "add_item.json")
+filename = "add_item.json"
+if os.path.isfile(filename):
+    obj = load_from_json_file(filename)
+else:
+    obj = []
+obj.extend(sys.argv[1:])
+save_to_json_file(obj, filename)
