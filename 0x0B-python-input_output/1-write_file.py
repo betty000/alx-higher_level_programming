0e5a8028523542680ce06725d91d94c13338b13a
Bethelhem Ashenafi <bettyashu5@gmail.com>
2022-10-25 12:58:03 -0700

    input_Output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""read_lines module"""
+"""
+File contains function that
+writes a string to text file and returns
+number of characters written TO IT.
+"""
 
 
-def read_lines(filename="", nb_lines=0):
-    """to read n lines from file, print entire thing if n <= 0 or > file"""
-    with open(filename, "r", encoding="utf-8") as content:
-        if not nb_lines:
-            print(content.read(), end="")
-            return
-        for eachline in content:
-            print(eachline, end="")
-            nb_lines -= 1
-            if not nb_lines:
-                break
+def write_file(filename="", text=""):
+    """
+    function to write to files
+    """
+    with open(filename, mode="w", encoding="utf-8") as file:
+        return (file.write(str(text)))
