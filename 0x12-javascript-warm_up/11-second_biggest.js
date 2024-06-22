7917026d37460d1f8bf3104915d0760a6e702d76
Bethelhem Ashenafi <bettyashu5@gmail.com>
2024-06-22 07:32:29 -0700

    JS

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,13 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) {
-  console.log('0');
+
+let args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.log(0);
+} else if (args.length === 1) {
+  console.log(0);
 } else {
-  const arr = process.argv.slice(2).map(Number);
-  const second = arr.sort(function (a, b) { return b - a; })[1];
-  console.log(second);
+  let nums = args.map(Number);
+  nums.sort((a, b) => b - a);
+  console.log(nums[1]);
 }
