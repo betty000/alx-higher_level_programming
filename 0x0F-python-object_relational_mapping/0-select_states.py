997de4a6598263ac46f831a03a0aba244a762493
Bethelhem Ashenafi <bettyashu5@gmail.com>
2024-06-24 00:57:41 -0700

    Python Object

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 """Script to select all states from database `hbtn_0e_0_usa`"""
 
-if __name__ == "__main__":
-    import MySQLdb
-    import sys
+import MySQLdb
+import sys
 
-    db = MySQLdb.connect(user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3])
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./script.py <username> <password> <database>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     for row in cur.fetchall():
